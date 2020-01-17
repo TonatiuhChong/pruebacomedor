a8ca0a185833fedd5e34be4d09062acb1d8ce753
Tonatiuh Chong Ramos <1754956@TCS.com>
2020-01-17 09:06:38 -0600

    uno

---
+++ clase1.js
@@ -0,0 +1,10 @@
+var nombre = 'Tona', apellido = 'Chong'
+var edad = 28
+
+edad = '28 años'
+
+console.log('Hola ' + nombre + ' ' + apellido)
+console.log('Tengo ' + edad)
+
+var peso = 75
+console.log('Y peso '+peso+'Kg')
